4370d8b1b20cb1161afd536f7372a01b7dc75e6c
Eduardo Rosales <eduardo.rosales@sodisa.online>
2023-08-17 07:29:48 -0600

    Created Controller with add, view all employees

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodeExitosProyect.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CodeExitosProyect.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly ShoppingWebAppContext _context;
+
+        public EmployeeController(ShoppingWebAppContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_context.Employees.ToList());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpPost("[action]")]
+        public async Task<OkResult> Add(NewEmployeeModel newEmployee)
+        {
+            try
+            {
+                Employee employee = new Employee();
+                employee.Name = newEmployee.Name;
+                employee.Address = newEmployee.Address;
+                employee.EmploymentDate = newEmployee.EmploymentDate;
+                employee.Telefhone = newEmployee.Telefhone;
+                employee.Type = newEmployee.Type;
+
+                _context.Employees.Add(employee);
+                await _context.SaveChangesAsync();
+
+                return await Task.Run(Ok);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
---
+++ Models/NewEmployeeModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeExitosProyect.Models
+{
+    public class NewEmployeeModel
+    {
+        public string? Name { get; set; }
+
+        public string? Telefhone { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? EmploymentDate { get; set; }
+        public int Type { get; set; }
+    }
+}
